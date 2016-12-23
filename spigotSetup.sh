3b594b62d5599a07e6054724433f0a86449a88d6
Desetude <thedankdesetude@gmail.com>
2016-12-23 18:28:12 +0000

    Travis setup

---
+++ spigotSetup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+mkdir target
+mkdir target/spigot
+cd target/spigot
+
+wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+java -jar BuildTools.jar --rev 1.11.2
+
+cd ..
+
+mkdir modularity
+cd modularity
+
+wget https://github.com/Desetude/Modularity/releases/download/1.0-SNAPSHOT/Modularity-1.0-SNAPSHOT.jar
+
+cd ../..
+
+mvn install:install-file -Dfile=target/spigot/spigot-1.11.2.jar -DgroupId=org.spigotmc -DartifactId=spigot -Dpackaging=jar -Dversion=1.11.2-R0.1-SNAPSHOT
+mvn install:install-file -Dfile=target/modularity/Modularity-1.0-SNAPSHOT.jar -DgroupId=com.harryfreeborough -DartifactId=modularity -Dpackaging=jar -Dversion=1.0-SNAPSHOT
